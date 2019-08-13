659fd929d1b521da558fc300118834df333c2051
NSLog0 <prachaya.su@gmail.com>
2019-08-14 02:04:20 +0700

    fix: warning security buffer deprecated

--- utils/base64.js
+++ utils/base64.js
@@ -1,14 +1,14 @@
 function encode(input) {
-    const buff = new Buffer(input, 'utf8')
+    const buff = new Buffer.from(input, 'utf8')
     return buff.toString('base64')
 }
 
 function decode(input) {
-    const buff = new Buffer(input, 'base64')
+    const buff = new Buffer.from(input, 'base64')
     return buff.toString('utf8')
 }
 
 module.exports = {
-    encode, 
+    encode,
     decode
 }
